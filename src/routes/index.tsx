4a30579f521ea64a35d75b06070638f6702bfcb4
takeyu1013 <yuto1013@gmail.com>
2025-11-09 21:06:19 +0900

    feat: Homeコンポーネントにフォームを追加し、スタイルを適用

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { convexQuery } from "@convex-dev/react-query";
+import { Button, Form, Input, Label, TextField } from "@heroui/react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { api } from "../../convex/_generated/api";
@@ -13,12 +14,19 @@ function Home() {
 	} = useSuspenseQuery(convexQuery(api.function.listNumber, { count: 10 }));
 
 	return (
-		<main>
+		<main className="container mx-auto">
 			<ul>
 				{numberList.map(({ id, value }) => (
 					<li key={id}>{value}</li>
 				))}
 			</ul>
+			<Form className="flex flex-col gap-y-4">
+				<TextField isRequired name="email" type="email">
+					<Label>Email</Label>
+					<Input />
+				</TextField>
+				<Button type="submit">Submit</Button>
+			</Form>
 		</main>
 	);
 }
