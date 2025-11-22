2fc2bb719d08708791889d6947a13ddeb413ece0
takeyu1013 <yuto1013@gmail.com>
2025-11-22 19:21:40 +0900

    chore: VercelのBun Runtimeに対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -8,7 +9,19 @@ export default defineConfig({
 	optimizeDeps: {
 		include: ["@clerk/tanstack-react-start", "cookie"],
 	},
-	plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), viteReact()],
+	plugins: [
+		nitro({
+			vercel: {
+				functions: {
+					runtime: "bun1.x",
+				},
+			},
+		}),
+		tailwindcss(),
+		tsConfigPaths(),
+		tanstackStart(),
+		viteReact(),
+	],
 	server: {
 		port: 3000,
 	},
