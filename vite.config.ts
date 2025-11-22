74797b5e96be82ed0ae9c0de99ae5d90f3de0956
takeyu1013 <yuto1013@gmail.com>
2025-11-22 18:55:12 +0900

    chore: 依存関係のバージョンを更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
 	optimizeDeps: {
 		include: ["@clerk/tanstack-react-start", "cookie"],
 	},
-	plugins: [
-		nitroV2Plugin(),
-		tailwindcss(),
-		tsConfigPaths(),
-		tanstackStart(),
-		viteReact(),
-	],
+	plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), viteReact()],
+	server: {
+		port: 3000,
+	},
 });
