d4d20148074cafefdc07fbee41ccb910da9d2b2b
takeyu1013 <yuto1013@gmail.com>
2026-02-11 21:21:40 +0900

    feat: 認証関連のコードを削除し、フォーム機能を追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,43 +1,65 @@
 import { convexQuery } from "@convex-dev/react-query";
+import { formOptions } from "@tanstack/react-form";
+import { useForm } from "@tanstack/react-form-start";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { getAuth, getSignInUrl } from "@workos/authkit-tanstack-react-start";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { createFileRoute } from "@tanstack/react-router";
+import { redirect } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
 
 import { api } from "../../convex/_generated/api";
 
+const options = formOptions({
+  defaultValues: {
+    text: "",
+  },
+});
+
+const handleForm = createServerFn({ method: "POST" })
+  .inputValidator((data) => {
+    if (!(data instanceof FormData)) {
+      throw redirect({ href: "/" });
+    }
+    return { text: data.get("text") };
+  })
+  .handler(async ({ data }) => {
+    console.log("data", data);
+    throw redirect({ href: "/" });
+  });
+
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: async () => {
-    const { user } = await getAuth();
-    const signInUrl = await getSignInUrl();
-    return { user, signInUrl };
-  },
 });
 
 function Home() {
   const { data } = useSuspenseQuery(convexQuery(api.function.listTable));
-  const { signInUrl } = Route.useLoaderData();
+  const { Field, Subscribe } = useForm(options);
   return (
     <main>
-      <Authenticated>
-        <ul>
-          {data.map(({ _id, text }) => (
-            <li key={_id}>{text}</li>
-          ))}
-        </ul>
-        <Link reloadDocument to="/sign-out">
-          Sign out
-        </Link>
-      </Authenticated>
-      <Unauthenticated>
-        <a href={signInUrl}>
-          <button>Sign in</button>
-        </a>
-      </Unauthenticated>
-      <div>
-        <Link to="/authenticated">Authenticated Page</Link>
-      </div>
+      <ul>
+        {data.map(({ _id, text }) => (
+          <li key={_id}>{text}</li>
+        ))}
+      </ul>
+      <form action={handleForm.url} method="post">
+        <Field name="text">
+          {({ handleChange, state }) => (
+            <input
+              className="border"
+              name="text"
+              onChange={(event) => handleChange(event.target.value)}
+              type="text"
+              value={state.value}
+            />
+          )}
+        </Field>
+        <Subscribe>
+          {({ canSubmit, isSubmitting }) => (
+            <button disabled={!canSubmit} type="submit">
+              {isSubmitting ? "..." : "Submit"}
+            </button>
+          )}
+        </Subscribe>
+      </form>
     </main>
   );
 }
