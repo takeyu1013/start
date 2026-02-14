e1a0983d399b368e819ae62641daede6c1527c7c
takeyu1013 <yuto1013@gmail.com>
2026-02-14 23:17:10 +0900

    feat: フォームのバリデーション機能を追加し、サーバーからのデータ取得を実装

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { convexQuery } from "@convex-dev/react-query";
-import { formOptions } from "@tanstack/react-form";
-import { useForm } from "@tanstack/react-form-start";
+import {
+  createServerValidate,
+  formOptions,
+  getFormData,
+  mergeForm,
+  ServerValidateError,
+  useForm,
+  useTransform,
+} from "@tanstack/react-form-start";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import { redirect } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
+import { setResponseStatus } from "@tanstack/react-start/server";
+import { z } from "zod";
 
 import { api } from "../../convex/_generated/api";
 
@@ -14,25 +22,52 @@ const options = formOptions({
   },
 });
 
+const serverValidate = createServerValidate({
+  ...options,
+  onServerValidate: z.object({
+    text: z.string().min(1),
+  }),
+});
+
 const handleForm = createServerFn({ method: "POST" })
-  .inputValidator((data) => {
+  .inputValidator((data: unknown) => {
     if (!(data instanceof FormData)) {
       throw redirect({ href: "/" });
     }
-    return { text: data.get("text") };
+    return data;
   })
   .handler(async ({ data }) => {
-    console.log("data", data);
+    try {
+      const validatedData = await serverValidate(data);
+      console.log("validatedData\n", validatedData);
+    } catch (error) {
+      if (error instanceof ServerValidateError) {
+        return error.response;
+      }
+      console.error("error\n", error);
+      setResponseStatus(500);
+    }
     throw redirect({ href: "/" });
   });
 
+const getFormDataFromServer = createServerFn({ method: "GET" }).handler(
+  async () => await getFormData(),
+);
+
 export const Route = createFileRoute("/")({
   component: Home,
+  loader: async () => ({
+    state: await getFormDataFromServer(),
+  }),
 });
 
 function Home() {
+  const { state } = Route.useLoaderData();
   const { data } = useSuspenseQuery(convexQuery(api.function.listTable));
-  const { Field, Subscribe } = useForm(options);
+  const { Field, Subscribe } = useForm({
+    ...options,
+    transform: useTransform((baseForm) => mergeForm(baseForm, state), [state]),
+  });
   return (
     <main>
       <ul>
@@ -42,15 +77,19 @@ function Home() {
       </ul>
       <form action={handleForm.url} method="post">
         <Field name="text">
-          {({ handleChange, state }) => (
-            <input
-              className="border"
-              name="text"
-              onChange={(event) => handleChange(event.target.value)}
-              type="text"
-              value={state.value}
-            />
-          )}
+          {({ handleChange, state }) => {
+            return (
+              <div>
+                <input
+                  className="border"
+                  name="text"
+                  onChange={(event) => handleChange(event.target.value)}
+                  type="text"
+                  value={state.value}
+                />
+              </div>
+            );
+          }}
         </Field>
         <Subscribe>
           {({ canSubmit, isSubmitting }) => (
